e68ec84aea0fb14784502a0b01a0bbfc84ae0ca0
Ertug Dilek <ertgdlk@gmail.com>
2020-10-15 14:49:37 +0300

    Jest framework installed. User Test file created

---
+++ config/test.js
@@ -0,0 +1,9 @@
+const testConfig = 
+{
+	"database":
+	{
+		"url": "mongodb://localhost:6000/up-test",
+	}
+}
+
+module.exports = testConfig
---
+++ tests/user.test.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const UserModel = require('../models/User');
+const userData = { nickkname: 'phybarin', name: 'Ertuğ', surname:'Dilek', email: 'ertgdlk@gmail.com',
+ password:'123456' }
+const bcrypt = require('bcrypt')
+
+describe('User Model Test', () => {
+
+    // It's just so easy to connect to the MongoDB Memory Server 
+    // By using mongoose.connect
+    beforeAll(async () => {
+        await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+            if (err) {
+                console.error(err)
+                process.exit(1)
+            }
+        });
+    });
+
+    it('create & save user successfully', async () => {
+        const validUser = new UserModel(userData)
+        const savedUser = await validUser.save()
+        // Object Id should be defined when successfully saved to MongoDB.
+        expect(savedUser._id).toBeDefined()
+        expect(savedUser.name).toBe(userData.name)
+        expect(savedUser.surname).toBe(userData.surname)
+        expect(savedUser.email).toBe(userData.email)
+        expect(bcrypt.compare(userData.password , savedUser.password)).toBe(true)
+    });
+})
