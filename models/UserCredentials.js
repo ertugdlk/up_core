792f30fbbc3025504e3bd42b1cb4c998132ae774
Ertug Dilek <ertgdlk@gmail.com>
2020-10-18 16:22:15 +0300

    usercredential model updated

--- models/UserCredentials.js
+++ models/UserCredentials.js
@@ -13,17 +13,11 @@ const CredentialsSchema = new Mongoose.Schema({
     },
     phone: {
         type: String,
-        validate: {
-          validator: function(v) {
-            return /\d{3}-\d{3}-\d{4}/.test(v);
-          },
-          message: props => `${props.value} is not a valid phone number!`
-        },
         required: [true, 'User phone number required']
     }
 })
 
-CredentialsSchema.pre('save', async function () {
+CredentialsSchema.pre('save', async function (next) {
     const credentials = this
     if (credentials.isModified('identityID')) {
         credentials.identityID = await encrypt(credentials.identityID)
