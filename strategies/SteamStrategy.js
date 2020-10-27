da8f606228c0dc9f7a04a2d1971e3a6de44d580d
Ertug Dilek <ertgdlk@gmail.com>
2020-10-27 09:36:30 +0300

    strategy added

---
+++ strategies/SteamStrategy.js
@@ -0,0 +1,19 @@
+const OpenIDStrategy = require('passport-openid').Strategy
+
+const SteamStrategy = new OpenIDStrategy({
+    providerURL:'http://steamcommunity.com/openid',
+    stateless:true,
+    returnURL: 'http://localhost:5000/',
+    realm: 'http://localhost:5000/',
+
+},
+    function(identifier, done) {
+        process.nextTick(function () {
+            const user = {
+                identifier,
+                steamId: identifier.match(/\d+$/)[0]
+            }
+
+            return done(user,null)
+        })
+    })
