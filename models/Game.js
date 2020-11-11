b2fc5149fe4a2d092f3cd4b0f470b9fdaca5ed7d
erce <canerce99@hotmail.com>
2020-11-11 19:58:35 +0300

    Game model change

--- models/Game.js
+++ models/Game.js
@@ -4,14 +4,21 @@ const Schema = Mongoose.Schema
 const GameSchema = new Mongoose.Schema({
     platform: { type: Schema.Types.ObjectId, ref: 'Platform' },
     name: {
-        type:String,
-        trim:true,
+        type: String,
+        trim: true,
         required: true
     },
-    appID: { 
+    appID: {
         type: String,
         required: true
+    },
+    ignNeeded: {
+        type: Boolean,
+        default: false
     }
-}, {versionKey: false})
+}, { versionKey: false })
 
 module.exports = Mongoose.model('Game', GameSchema)
+
+
+//bool ignNeeded oyunun ign gerektirip gerektirmediği 
