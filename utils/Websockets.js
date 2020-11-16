7cdc7ac8759d4ac066a16b8ac6edf123a0980c1a
Ertug Dilek <ertgdlk@gmail.com>
2020-11-16 11:47:12 +0300

    Websocket with redis functions

--- utils/Websockets.js
+++ utils/Websockets.js
@@ -1,12 +1,18 @@
+const {createRoom, closeRoom , getRoom} = require('./RedisUtil')
 
 class Websockets {
     connection(client) {
-        client.on("join" , (msg) => {
-            console.log('connected' + msg)
+        client.on("create" , (gameData) => {
+            const returnMessage = createRoom(client.id, gameData)
+            client.emit("createReturn" , (returnMessage))
         })
 
-        client.on("disconnect", () => {
-            console.log('User disconnected')
+        client.on("join" , (socketId) => {
+            client.to("socketId")
+        })
+
+        client.on("close", () => {
+            closeRoom(client.id)
           });
     }
 }
