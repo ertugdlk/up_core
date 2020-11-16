b84ae324d9313b35c48857677567480f8fad6a9a
Ertug Dilek <ertgdlk@gmail.com>
2020-11-16 21:29:55 +0300

    Websocket edit

--- utils/Websockets.js
+++ utils/Websockets.js
@@ -2,8 +2,11 @@ const {createRoom, closeRoom , getRoom} = require('./RedisUtil')
 
 class Websockets {
     connection(client) {
+        console.log('a user connected')
+
         client.on("create" , (gameData) => {
             const returnMessage = createRoom(client.id, gameData)
+            console.log(client.id)
             client.emit("createReturn" , (returnMessage))
         })
 
