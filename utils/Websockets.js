a14ded63b243baa94e8bab4f6f0d3fe12463a2c5
Ertug Dilek <ertgdlk@gmail.com>
2020-11-17 15:45:32 +0300

    last changes

--- utils/Websockets.js
+++ utils/Websockets.js
@@ -1,26 +1,13 @@
 const { createRoom, closeRoom, getRoom, getRooms } = require('./RedisUtil')
 const _ = require('lodash')
-
+var list = []
+var rooms = []
 
 class Websockets {
     connection(client) {
-        console.log('a user connected')
-        var list = []
-        var rooms = []
-
-        client.emit("getRooms", () => {
-            getRooms((err,data) => {
-                if(data){
-                    list = _.flattenDeep(data)
-                }
-            })
-
-            list.map(key => {
-                getRoom(key, (err, result) => {
-                    rooms.push(result) 
-                })
-            })
-        })
+        console.log(client.id + 'user connected')
+        
+        client.emit("rooms", "deneme")
 
         client.on("create", (gameData) => {
             const returnMessage = createRoom(client.id, gameData)
