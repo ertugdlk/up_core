1368abdb1e064455c19469866998526be1bb005b
Ertug Dilek <ertgdlk@gmail.com>
2020-12-09 20:52:13 +0300

    RCONutil bug fix

--- utils/RCONutil.js
+++ utils/RCONutil.js
@@ -1,15 +1,23 @@
 const {Rcon} = require('rcon-client')
 const Config = require('config')
 
-const _rcon = await Rcon.connect({
+const _rcon = Rcon.connect({
     host: process.env.RCON_HOST,
     port: Config.get('rcon.port'),
     password: process.env.RCON_PASS
 })
 
 async function gameStatus(){
-    const response = await Promise.all([_rcon.send("get5_status")])
-    return response
+    try{
+        const rcon = new Rcon({ host: process.env.RCON_HOST, port: Config.get('rcon.port'), password: process.env.RCON_PASS })
+        await rcon.connect()
+        const response = await Promise.all([rcon.send("get5_status")])
+        return response
+    }
+    catch(error)
+    {
+        throw error
+    }
 }
 
 module.exports = {
