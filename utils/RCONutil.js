c2386459c733c346039ac327da23224b3b556ec9
Ertug Dilek <ertgdlk@gmail.com>
2020-12-20 12:13:40 +0300

    rcon settings updated

--- utils/RCONutil.js
+++ utils/RCONutil.js
@@ -2,7 +2,10 @@ const {Rcon} = require('rcon-client')
 const Config = require('config')
 const steamid = require('@node-steam/id')
 const GameRoom = require('../models/GameRoom')
+const Detail = require('../models/Detail')
+const User = require('../models/User')
 const _ = require('lodash')
+const steamid = require('@node-steam/id')
 
 var matchconfig = require('./matchconfig.json')
 const rcon = new Rcon({ host: process.env.RCON_HOST, port: Config.get('rcon.port'), password: process.env.RCON_PASS })
@@ -57,16 +60,36 @@ async function matchSettings(host){
     {
         const room = await GameRoom.findOne({host:host})
 
-        const team1 = _.find(room.users , function(roomUser){
+        const team1 = _.filter(room.users , function(roomUser){
             return roomUser.team == 1
         })
-        const team2 = _.find(room.users , function(roomUser){
+        const team2 = _.filter(room.users , function(roomUser){
             return roomUser.team == 2
         })
+        var gameteam1 = [];
+        var gameteam2 = [];
+
+        team1.map( (member) => {
+            const user = await User.findOne({ nickname: member.nickname})
+            const detail = await Detail.findOne({user: user._id, platform: '5f9a84fca1f0c0b83de7d696'})
+            const newId = new steamid.ID(detail.uniqueID)
+            const convertedId = newId.get2()
+            gameteam1.push(convertedId)
+        })
+
+
+        team2.map( (member) => {
+            const user = await User.findOne({ nickname: member.nickname})
+            const detail = await Detail.findOne({user: user._id, platform: '5f9a84fca1f0c0b83de7d696'})
+            const newId = new steamid.ID(detail.uniqueID)
+            const convertedId = newId.get2()
+            gameteam2.push(convertedId)
+        })
+
 
         matchconfig.matchid = room.roomId
-        matchconfig.team1.players = team1
-        matchconfig.team2.players = team2
+        matchconfig.team1.players = gameteam1
+        matchconfig.team2.players = gameteam2
         matchconfig.maplist = room.settings.map
         matchconfig.players_per_team = team1.length
         matchconfig.min_players_to_ready = team1.length * 2
