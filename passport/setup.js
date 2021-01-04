8f0373a8c7282613c648b29c2a2b8248d9a17593
Ertug Dilek <ertgdlk@gmail.com>
2021-01-04 11:53:07 +0300

    local url change test

--- passport/setup.js
+++ passport/setup.js
@@ -1,21 +1,21 @@
 const passport = require('passport')
-    , util = require('util')
-    , SteamStrategy = require('passport-steam').Strategy
+  , util = require('util')
+  , SteamStrategy = require('passport-steam').Strategy
 
-passport.serializeUser(function(user, done) {
-        done(null, user)
-      })
-      
-passport.deserializeUser(function(obj, done) {
-        done(null, obj)
-      })
+passport.serializeUser(function (user, done) {
+  done(null, user)
+})
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj)
+})
 
 passport.use(new SteamStrategy({
-    returnURL: 'https://test.unknownpros.com/steam/redirect',
-    realm: 'https://test.unknownpros.com/',
-    apiKey: process.env.STEAM_APIKEY
-  },
-  function(identifier, profile, done) {
+  returnURL: 'http://localhost:5000/steam/redirect',
+  realm: 'http://localhost:5000/',
+  apiKey: process.env.STEAM_APIKEY
+},
+  function (identifier, profile, done) {
     process.nextTick(function () {
       profile.identifier = identifier
       return done(null, profile)
