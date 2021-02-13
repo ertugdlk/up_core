7dbe5a581325d1d22b16da5bce684a0346fbcb18
ertugdlk <ertgdlk@gmail.com>
2021-02-13 21:33:40 +0300

    passport steam env variable settings

--- passport/setup.js
+++ passport/setup.js
@@ -11,8 +11,8 @@ passport.deserializeUser(function (obj, done) {
 })
 
 passport.use(new SteamStrategy({
-  returnURL: 'https://test.unknownpros.com:5000/steam/redirect',
-  realm: 'https://test.unknownpros.com:5000/',
+  returnURL: process.env.BASE_URL + 'steam/redirect',
+  realm: process.env.BASE_URL,
   apiKey: process.env.STEAM_APIKEY
 },
   function (identifier, profile, done) {
